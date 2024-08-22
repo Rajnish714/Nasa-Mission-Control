89f2a1a43dcc5c23c3ce851bd8cba9d200ed638f
GameRix <srajnish714@gmail.com>
2024-08-22 18:27:04 +0530

    fix build added

--- client/src/hooks/useLaunches.js
+++ client/src/hooks/useLaunches.js
@@ -8,7 +8,6 @@ function useLaunches(onSuccessSound, onAbortSound, onFailureSound) {
 
   const getLaunches = useCallback(async () => {
     const fetchedLaunches = await httpGetLaunches();
-
     saveLaunches(fetchedLaunches);
   }, []);
 
