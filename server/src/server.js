c1cb207a717731addff6d46603f6a178b095e677
GameRix <srajnish714@gmail.com>
2024-09-20 03:44:24 +0530

    jest test updated

--- server/src/server.js
+++ server/src/server.js
@@ -1,26 +1,12 @@
 const http = require("http");
 const app = require("./app");
-const mongoose = require("mongoose");
-const MONGO_URL =
-  "mongodb://localhost:27017/nasa-api?readPreference=primary&ssl=false&directConnection=true";
+
 const {loadPlanetData} = require("./modules/planets.modules");
 
 const server = http.createServer(app);
 
-mongoose.connection.once("open", () => {
-  console.log("mongoDB is ready!");
-});
-mongoose.connection.on("error", (err) => {
-  console.log(err);
-});
-
 async function startServer() {
-  await mongoose.connect(MONGO_URL, {
-    // useNewUrlParser: true,
-    //useFindAndModify: false,
-    //useCreateIndex: true,
-    // useUnifiedTopology: true,
-  });
+  await mongoConnect;
   await loadPlanetData();
   server.listen(8000, () => {
     console.log(`server has started on ${8000}...`);
