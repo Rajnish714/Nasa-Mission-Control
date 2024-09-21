b2e51cf37c3c50256aee22be8aebf48aab31924c
GameRix <srajnish714@gmail.com>
2024-09-21 14:30:09 +0530

    api v1

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,12 @@
 const http = require("http");
 const app = require("./app");
-
+const {mongoConnect} = require("./services/mongo");
 const {loadPlanetData} = require("./modules/planets.modules");
 
 const server = http.createServer(app);
 
 async function startServer() {
-  await mongoConnect;
+  await mongoConnect();
   await loadPlanetData();
   server.listen(8000, () => {
     console.log(`server has started on ${8000}...`);
